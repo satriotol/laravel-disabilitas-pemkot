8f0ac28fcdb825500e32824ab1c9330d6b360942
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-08-26 09:32:04 +0700

    product content

--- database/migrations/2022_08_26_021125_create_products_table.php
+++ database/migrations/2022_08_26_021125_create_products_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->bigInteger('price');
+            $table->bigInteger('stock');
+            $table->longText('description');
             $table->timestamps();
         });
     }
