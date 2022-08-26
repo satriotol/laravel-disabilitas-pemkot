b39e08ba72a8c55fe5c495526997bf0764b5b931
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-08-26 10:27:29 +0700

    products

--- database/migrations/2022_08_26_032603_create_products_table.php
+++ database/migrations/2022_08_26_032603_create_products_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->bigInteger('price');
-            $table->bigInteger('stock');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('user_id');
+            $table->bigInteger('discount_price')->nullable();
             $table->longText('description');
             $table->timestamps();
         });
