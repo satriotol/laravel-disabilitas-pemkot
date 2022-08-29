19588dab5ec1adedab05d13e3a36a2f3c30c265e
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-08-29 08:40:28 +0700

    product model

---
+++ app/Models/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['category_id', 'user_id', 'name', 'price', 'discount_price', 'description', 'stock'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+    public function product_images()
+    {
+        return $this->hasMany(ProductImage::class, 'product_id', 'id');
+    }
+}
