a4e454fe86d733e5bdc0d58f3d55262799cb5ef2
HannaFasya <hanna.firdausiah20@gmail.com>
2022-08-29 09:15:04 +0700

    model product images

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class ProductImage extends Model
 {
     use HasFactory;
+
+    protected $table = 'product_images';
+
+    protected $fillable = ['product_id', 'image'];
+
+    public function product (){
+        return $this->hasMany('App\Models\Product', 'product_id');
+    }
+
 }
