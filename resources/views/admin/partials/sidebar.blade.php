e7a39a61ab5d8f9dd1e0bca806c0f2eb3c18cdfb
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-08-30 22:49:06 +0700

    sidebar

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -1,55 +1,50 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-2">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">PEMKOT SEMARANG</span>
+    <a href="#" class="brand-link">
+        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+            style="opacity: .8">
+        <span class="brand-text font-weight-light">PEMKOT SEMARANG</span>
     </a>
 
-    <!-- Sidebar -->
     <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+            </div>
         </div>
-        <div class="info">
-          <a href="#" class="d-block">{{Auth::user()->name}}</a>
-        </div>
-      </div>
 
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-          <li class="nav-item">
-            <a href="{{ route('tabel.1') }}" class="nav-link active">
-              <i class="nav-icon fas fa-copy"></i>
-              <p>
-                Tabel 1
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="{{ route('tabel.2') }}" class="nav-link">
-              <i class="nav-icon fas fa-copy"></i>
-              <p>
-                Tabel 2
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="{{ route('tabel.3') }}" class="nav-link">
-              <i class="nav-icon fas fa-copy"></i>
-              <p>
-                Tabel 3
-              </p>
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                data-accordion="false">
+                <li class="nav-item">
+                    <a href="{{ route('tabel.1') }}" class="nav-link active">
+                        <i class="nav-icon fas fa-copy"></i>
+                        <p>
+                            Tabel 1
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('tabel.2') }}" class="nav-link">
+                        <i class="nav-icon fas fa-copy"></i>
+                        <p>
+                            Tabel 2
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('tabel.3') }}" class="nav-link">
+                        <i class="nav-icon fas fa-copy"></i>
+                        <p>
+                            Tabel 3
+                        </p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
     </div>
-    <!-- /.sidebar -->
-  </aside>
+</aside>
