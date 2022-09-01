35c3e6b4d786492154bd5a985ed764ccbc175f4b
dwiriad <99604497+dwiriad@users.noreply.github.com>
2022-09-01 09:18:27 +0700

    slider

---
+++ resources/views/socialmedia/index.blade.php
@@ -0,0 +1,53 @@
+@extends('admin.layouts.main')
+
+
+@section('container')
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Kategori</h1>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+    <section class="content">
+
+        <!-- Default box -->
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Tabel Kategori</h3>
+            </div>
+            <div class="card-body">
+                <div class="text-right mb-2">
+                    <a href="{{ route('kategori.create') }}"><button type="button" class="btn bg-gradient-primary">Create</button></a>
+                </div>
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th style="width: 50px">Id</th>
+                            <th>Nama</th>
+                            <th>Image</th>
+                            <th style="width: 150px">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            @foreach ($kategori as $k)
+                            <td>{{ $k->id }}</td>
+                            <td>{{ $k->name }}</td>
+                            <td>
+                                <img src="{{ asset('gambar/'.$k->image) }}" alt="" style="width:80px;height:50px;display:block;margin-right:auto;margin-left:auto;">
+                            </td>
+                            <td>
+                                <a href="{{ route('kategori.edit', $k->id) }}"><button type="button" name="edit" class="btn btn-sm bg-gradient-warning">Edit</button></a>
+                                <a href="{{ route('kategori.delete', $k->id) }}"><button type="button" name="delete" class="btn btn-sm bg-gradient-danger"onclick="return confirm('Anda yakin ingin menghapus data ini?')">Hapus</button></a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,8 @@ Route::group(['middleware' => ['auth']], function () {
     Route::get('/slider', 'App\Http\Controllers\SliderController@index')->name('slider');
     Route::get('/slider/create', 'App\Http\Controllers\SliderController@create')->name('slider.create');
     Route::post('/slider/store', 'App\Http\Controllers\SliderController@store')->name('slider.store');
+    Route::get('/slider/edit/{id}', 'App\Http\Controllers\SliderController@edit')->name('Slider.edit');
+
 
 
 });
