76310d11b448dc866ea714e8a0a82e9f84d68791
adisti khairunnisa <adistikhairunnisaa@gmail.com>
2022-09-01 09:44:29 +0700

    controller slider lagi..

--- app/Http/Controllers/SliderController.php
+++ app/Http/Controllers/SliderController.php
@@ -15,10 +15,12 @@ class SliderController extends Controller
         $slider = Slider::orderBy('id', 'asc')->paginate(5);
         return view('slider.slide', compact('slider'));
     }
+
     public function create()
     {
         return view('slider.create');
     }
+
     public function store(Request $request)
     {
         $this->validate($request,[
@@ -37,6 +39,36 @@ class SliderController extends Controller
         Session::flash('flash_message', 'Slider berhasil disimpan');
 
     	return redirect('/slider');
+    }
+
+    public function edit($id)
+    {
+        $slider = Slider::Find($id);
+        return view('slider.edit', compact('slider'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $slider = Slider::find($id);
+        if ($request->image) {
+            $foto_slider = $request->image;
+            $nama_file = time() . '.' . $foto_slider->getClientOriginalExtension();
+            $foto_slider->move('gambar/', $nama_file);
+            $slider->image = $nama_file;
+        }
+        $slider->name = $request->name;
+        $slider->update();
+
+        Session::flash('flash_message', 'Data Slider berhasil diupdate');
+
+        return redirect('/slider');
+    }
+
+    public function delete(Slider $slider)
+    {
+        $slider->delete();
+        Session::flash('flash_message', 'Data slider berhasil dihapus');
+        return redirect('/slider');
     }
 
 }
