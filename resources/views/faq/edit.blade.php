02183733d7097972259505dc21d3e4453a128ec0
HannaFasya <hanna.firdausiah20@gmail.com>
2022-09-01 10:32:56 +0700

    view edit FAQ

---
+++ resources/views/faq/edit.blade.php
@@ -0,0 +1,53 @@
+@extends('admin.layouts.main')
+
+@section('container')
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Kategori</h1>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <!-- left column -->
+            <div class="col-md-12">
+              <!-- general form elements -->
+              <div class="card card-primary">
+                <div class="card-header">
+                  <h3 class="card-title">Update FAQ</h3>
+                </div>
+                <!-- /.card-header -->
+                <!-- form start -->
+                <form method="POST" action="{{ route('faq.update', $faq->id) }}" enctype="multipart/form-data">
+                @csrf
+                  <div class="card-body">
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Question</label>
+                      <input type="text" name="name" class="form-control" id="exampleInputEmail1" value="{{ $faq->question }}">
+                    </div>
+                    <div class="card-body">
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Answer</label>
+                      <input type="text" name="name" class="form-control" id="exampleInputEmail1" value="{{ $faq->answer }}">
+                    </div>
+                  </div>
+                  <!-- /.card-body -->
+
+                  <div class="card-footer">
+                    <button type="submit" class="btn btn-primary">Update</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <!--/.col (right) -->
+          </div>
+          <!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </section>
+      <!-- /.content -->
+@endsection
