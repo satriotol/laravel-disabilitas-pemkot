ba1efe123698bbea7302166eef18de46b208b288
dwiriad <99604497+dwiriad@users.noreply.github.com>
2022-09-08 12:20:06 +0700

    dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\User;
+use App\Models\Product;
+use App\Models\SocialMedia;
 
 class AdminController extends Controller
 {
@@ -19,6 +23,10 @@ class AdminController extends Controller
     }
 
     public function dashboard(){
-        return view('dashboard1');
+        $user = User::count();
+        $product = Product::count();
+        $kategori = Category::count();
+        $sosmed = SocialMedia::count();
+        return view('dashboard1', compact('user', 'product', 'kategori', 'sosmed'));
     }
 }
--- resources/views/dashboard1.blade.php
+++ resources/views/dashboard1.blade.php
@@ -19,14 +19,14 @@
               <!-- small box -->
               <div class="small-box bg-info">
                 <div class="inner">
-                  <h3>150</h3>
+                  <h3>{{ $user }}</h3>
 
-                  <p>New Orders</p>
+                  <p>User</p>
                 </div>
                 <div class="icon">
                   <i class="ion ion-bag"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="{{ route('user') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
               </div>
             </div>
             <!-- ./col -->
@@ -34,14 +34,14 @@
               <!-- small box -->
               <div class="small-box bg-success">
                 <div class="inner">
-                  <h3>53<sup style="font-size: 20px">%</sup></h3>
+                  <h3>{{$kategori}}</h3>
 
-                  <p>Bounce Rate</p>
+                  <p>Category</p>
                 </div>
                 <div class="icon">
                   <i class="ion ion-stats-bars"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="{{ route('kategori') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
               </div>
             </div>
             <!-- ./col -->
@@ -49,14 +49,14 @@
               <!-- small box -->
               <div class="small-box bg-warning">
                 <div class="inner">
-                  <h3>44</h3>
+                  <h3>{{$product}}</h3>
 
-                  <p>User Registrations</p>
+                  <p>Product</p>
                 </div>
                 <div class="icon">
                   <i class="ion ion-person-add"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="{{ route('product') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
               </div>
             </div>
             <!-- ./col -->
@@ -64,14 +64,14 @@
               <!-- small box -->
               <div class="small-box bg-danger">
                 <div class="inner">
-                  <h3>65</h3>
+                  <h3>{{$sosmed}}</h3>
 
-                  <p>Unique Visitors</p>
+                  <p>Social Media</p>
                 </div>
                 <div class="icon">
                   <i class="ion ion-pie-graph"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="{{ route('socialmedia') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
               </div>
             </div>
             <!-- ./col -->
