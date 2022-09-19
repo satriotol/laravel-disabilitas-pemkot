19a2860cdbfebc69064214bd4b5968ae437c83a6
adisti khairunnisa <adistikhairunnisaa@gmail.com>
2022-09-19 11:44:01 +0700

    navbar

--- resources/views/interface/partial/navbar.blade.php
+++ resources/views/interface/partial/navbar.blade.php
@@ -9,11 +9,6 @@
             <div class="shop-menu">
                 <ul>
                     <li>
-                        <a href="compare.html">
-                            <img src="{{asset('img/Button Jual-03.png')}}" style="width: 110px; height: 56px;" alt="Semarang Care Official">
-                        </a>
-                    </li>
-                    <li>
                         <a href="{{ route('cart') }}">
                             <img src="{{asset('img/ikon keranjang-01.png')}}" style="width: 57px; height: 54px;" alt="Semarang Care Official">
                         </a>
