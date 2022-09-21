c273cfa8c4c810eca97c0214afc0a437659bf039
adisti khairunnisa <adistikhairunnisaa@gmail.com>
2022-09-21 11:39:31 +0700

    fixing interface

--- app/Http/Controllers/InterfaceController.php
+++ app/Http/Controllers/InterfaceController.php
@@ -5,11 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Faq;
 use App\Models\Slider;
 use App\Models\About;
-<<<<<<< HEAD
 use App\Models\Category;
-=======
 use App\Models\SocialMedia;
->>>>>>> 5e583cecec836b939c980c2c39cc75a9c7237fee
 use Illuminate\Http\Request;
 
 class InterfaceController extends Controller
@@ -19,13 +16,8 @@ class InterfaceController extends Controller
         $sliders = Slider::all();
         $abouts = About::all();
         $faqs = Faq::all();
-<<<<<<< HEAD
         $categories = Category::all();
         return view('interface.index', compact('sliders', 'abouts', 'faqs', 'categories'));
-=======
-        $socialmedia = SocialMedia::all();
-        return view('interface.index', compact('sliders', 'abouts', 'faqs', 'socialmedia'));
->>>>>>> 5e583cecec836b939c980c2c39cc75a9c7237fee
 
 
 
