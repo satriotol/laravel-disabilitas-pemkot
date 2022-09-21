2587b9e20a121495d07f8011dc2774bf1e8a28de
HannaFasya <hanna.firdausiah20@gmail.com>
2022-09-21 10:14:39 +0700

    fixing

--- app/Http/Controllers/InterfaceController.php
+++ app/Http/Controllers/InterfaceController.php
@@ -12,17 +12,11 @@ class InterfaceController extends Controller
     public function index()
     {
         $sliders = Slider::all();
-<<<<<<< HEAD
         $abouts = About::all();
         return view('interface.index', compact('sliders', 'abouts'));
 
 
 
-=======
-        return view('interface.index', compact('sliders'));
-        $faqs = Faq::all();
-        return view('interface.index', compact('faqs'));
->>>>>>> 2d66db75ac1899d5029a85b6f179a14380a3b6f6
     }
 
     public function cart()
