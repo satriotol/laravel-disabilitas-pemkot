f6e73965bbdf978ee7e801368f857284bb48e373
HannaFasya <hanna.firdausiah20@gmail.com>
2022-09-27 15:01:03 +0700

    fixed navbar

--- resources/views/interface/partial/navbar.blade.php
+++ resources/views/interface/partial/navbar.blade.php
@@ -10,12 +10,15 @@
                 <ul>
                     <li>
                         <a href="{{ route('cart') }}">
-                            <img src="{{asset('img/ikon keranjang-01.png')}}" style="width: 57px; height: 54px;" alt="Semarang Care Official">
+                            {{-- <img src="{{asset('img/ikon keranjang-01.png')}}" style="width: 57px; height: 54px;" alt="Semarang Care Official"> --}}
+                            <button class="btn-jual">Jual</button>
                         </a>
+
                     </li>
-                    <li>
+                    <li class="login">
                         <a href="{{ route('login') }}">
-                            <img src="{{asset('img/ikon user-02.png')}}" style="width: 57px; height: 54px;" alt="Semarang Care Official">
+                            {{-- <img src="{{asset('img/ikon user-02.png')}}" style="width: 57px; height: 54px;" alt="Semarang Care Official"> --}}
+                            <button class="btn-login">Masuk</button>
                         </a>
                     </li>
                 </ul>
