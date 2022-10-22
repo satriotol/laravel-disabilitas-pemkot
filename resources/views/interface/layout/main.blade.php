88ef011624923b69bb9c8742dec3f83ea68558cf
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-10-22 09:46:38 +0700

    fixing footer

--- resources/views/interface/layout/main.blade.php
+++ resources/views/interface/layout/main.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,37 +9,39 @@
     <link rel="icon" href="{{ asset('img/logook.png') }}" type="image/x-icon">
     <title>Semarang Care Official</title>
 
-    <link rel="stylesheet" href="css/fontawesome.min.css">
+    <link rel="stylesheet" href="{{ asset('css 1/fontawesome.min.css') }}">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="assets/lib/font-awesome/css/font-awesome.css">
-    <link rel="stylesheet" href="{{asset('css 1/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{asset('css 1/ion.rangeSlider.css')}}">
-    <link rel="stylesheet" href="{{asset('css 1/ion.rangeSlider.skinFlat.css')}}">
-    <link rel="stylesheet" href="{{asset('css 1/jquery.bxslider.css')}}">
-    <link rel="stylesheet" href="{{asset('css 1/jquery.fancybox.css')}}">
-    <link rel="stylesheet" href="{{asset('css 1/flexslider.css')}}">
-    <link rel="stylesheet" href="{{asset('css 1/swiper.css')}}">
-    <link rel="stylesheet" href="{{asset('css 1/style.css')}}">
-    <link rel="stylesheet" href="{{asset('css 1/media.css')}}">
+    <link rel="stylesheet" href="{{ asset('css 1/assets/lib/font-awesome/css/font-awesome.css') }}">
+    <link rel="stylesheet" href="{{ asset('css 1/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css 1/ion.rangeSlider.css') }}">
+    <link rel="stylesheet" href="{{ asset('css 1/ion.rangeSlider.skinFlat.css') }}">
+    <link rel="stylesheet" href="{{ asset('css 1/jquery.bxslider.css') }}">
+    <link rel="stylesheet" href="{{ asset('css 1/jquery.fancybox.css') }}">
+    <link rel="stylesheet" href="{{ asset('css 1/flexslider.css') }}">
+    <link rel="stylesheet" href="{{ asset('css 1/swiper.css') }}">
+    <link rel="stylesheet" href="{{ asset('css 1/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css 1/media.css') }}">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis">
     <!-- style css -->
-    <link rel="stylesheet" type="text/css" href="{{asset('tentang/css/style.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('tentang/css/style.css') }}">
     <!-- Responsive-->
-    <link rel="stylesheet" href="{{asset('tentang/css/responsive.css')}}">
+    <link rel="stylesheet" href="{{ asset('tentang/css/responsive.css') }}">
     <!-- Scrollbar Custom CSS -->
-    <link rel="stylesheet" href="{{asset('tentang/css/jquery.mCustomScrollbar.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('tentang/css/jquery.mCustomScrollbar.min.css') }}">
     <!-- Tweaks for older IEs-->
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/6.2.0/css/font-awesome.css">
     <!-- owl stylesheets -->
-    <link rel="stylesheet" href="{{asset('tentang/css/owl.carousel.min.css')}}">
-    <link rel="stylesheet" href="{{asset('tentang/css/owl.theme.defaSelt.min.css')}}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+    <link rel="stylesheet" href="{{ asset('tentang/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('tentang/css/owl.theme.defaSelt.min.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css"
+        media="screen">
     <!-- FAQ -->
-    <link rel="stylesheet" href="{{asset('css 1/faq.css')}}">
+    <link rel="stylesheet" href="{{ asset('css 1/faq.css') }}">
     {{-- <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}"> --}}
 
 </head>
+
 <body>
     <!-- Header - start -->
     <header class="header">
@@ -64,7 +67,7 @@
             <div class="row">
                 <div class="f-menu">
                     <a href="{{ route('home') }}">
-                        <img src="{{asset('img/S-care logo part 2-03.png')}}" alt="S-care">
+                        <img src="{{ asset('img/S-care logo part 2-03.png') }}" alt="S-care">
                     </a>
                 </div>
                 <div class="f-menu">
@@ -72,11 +75,11 @@
                         Home
                     </h3>
                     <ul class="nav nav-pills nav-stacked">
-                        <li><a href="{{ route('home') }}#kategori-bab">Kategori</a></li>
-                        <li><a href="{{ route('home') }}#tentang-bab">Produk</a></li>
-                        <li><a href="{{ route('home') }}#faq-bab">FAQ</a></li>
-                        <li><a href="{{ route('home') }}#kontakkami-bab">Kontak kami</a></li>
-                        <li><a href="{{ route('home') }}#kontakkami-bab">Tentang</a></li>
+                        <li><a href="#kategori-bab">Kategori</a></li>
+                        <li><a href="#tentang-bab">Produk</a></li>
+                        <li><a href="#faq-bab">FAQ</a></li>
+                        <li><a href="#kontakkami-bab">Kontak kami</a></li>
+                        <li><a href="#kontakkami-bab">Tentang</a></li>
                     </ul>
                 </div>
                 <div class="f-menu">
@@ -85,7 +88,7 @@
                     </h3>
                     <ul class="nav nav-pills nav-stacked">
                         @foreach ($categories as $cate)
-                        <li><a href="{{route('product-gallery')}}">{{$cate->name}}</a></li>
+                            <li><a href="{{ route('product-gallery') }}">{{ $cate->name }}</a></li>
                         @endforeach
                     </ul>
                 </div>
@@ -95,10 +98,10 @@
                     </h3>
                     <ul class="nav nav-pills nav-stacked">
                         @foreach ($kontak_kami as $kk)
-                        <li><a> Telp: {{$kk->telp}}</a></li>
-                        <li><a> Kode Pos: {{$kk->kode_pos}}</a></li>
-                        <li><a> Alamat: {{$kk->alamat}}</a></li>
-                        <li><a> E-mail: {{$kk->email}}</a></li>
+                            <li><a> Telp: {{ $kk->telp }}</a></li>
+                            <li><a> Kode Pos: {{ $kk->kode_pos }}</a></li>
+                            <li><a> Alamat: {{ $kk->alamat }}</a></li>
+                            <li><a> E-mail: {{ $kk->email }}</a></li>
                         @endforeach
                     </ul>
                 </div>
@@ -118,16 +121,17 @@
                 <div class="row">
                     <ul class="social-icons nav navbar-nav" style="color: #fff;">
                         @foreach ($socialmedia as $sm)
-                        <li >
-                            <a href="{{$sm->url}}" rel="nofollow" target="_blank">
-                                <img src="{{asset('gambar/' . $sm->image)}}" style="width:35px; height:35px; margin-right: 3px;">
-                            </a>
-                        </li>
-
+                            <li>
+                                <a href="{{ $sm->url }}" rel="nofollow" target="_blank">
+                                    <img src="{{ asset('gambar/' . $sm->image) }}"
+                                        style="width:35px; height:35px; margin-right: 3px;">
+                                </a>
+                            </li>
                         @endforeach
                     </ul>
                     <div class="footer-copyright">
-                        <i><a href="https://semarangkota.go.id/" style="color: #fff;">Pemkot Semarang</a></i> ©Copyright 2022
+                        <i><a href="https://semarangkota.go.id/" style="color: #fff;">Pemkot Semarang</a></i> ©Copyright
+                        2022
                     </div>
                 </div>
             </div>
@@ -142,31 +146,32 @@
     <script>
         var navBar = document.getElementById("navBar");
         window.onscroll = function() {
-          if (window.scrollY > 22) {
-            navBar.classList.add("scrolled");
-          } else {
-            navBar.classList.remove("scrolled");
-          }
+            if (window.scrollY > 22) {
+                navBar.classList.add("scrolled");
+            } else {
+                navBar.classList.remove("scrolled");
+            }
         };
-      </script>
+    </script>
 
-    <script src="{{asset('js/jquery-1.11.2.min.js')}}"></script>
-    <script src="{{asset('js/jquery.bxslider.min.js')}}"></script>
-    <script src="{{asset('js/fancybox/fancybox.js')}}"></script>
-    <script src="{{asset('js/fancybox/helpers/jquery.fancybox-thumbs.js')}}"></script>
-    <script src="{{asset('js/jquery.flexslider-min.js')}}"></script>
-    <script src="{{asset('js/swiper.jquery.min.js')}}"></script>
-    <script src="{{asset('js/jquery.waypoints.min.js')}}"></script>
-    <script src="{{asset('js/progressbar.min.js')}}"></script>
-    <script src="{{asset('js/ion.rangeSlider.min.js')}}"></script>
-    <script src="{{asset('js/chosen.jquery.min.js')}}"></script>
-    <script src="{{asset('js/jQuery.Brazzers-Carousel.js')}}"></script>
-    <script src="{{asset('js/plugins.js')}}"></script>
-    <script src="{{asset('js/main.js')}}"></script>
+    <script src="{{ asset('js/jquery-1.11.2.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.bxslider.min.js') }}"></script>
+    <script src="{{ asset('js/fancybox/fancybox.js') }}"></script>
+    <script src="{{ asset('js/fancybox/helpers/jquery.fancybox-thumbs.js') }}"></script>
+    <script src="{{ asset('js/jquery.flexslider-min.js') }}"></script>
+    <script src="{{ asset('js/swiper.jquery.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.waypoints.min.js') }}"></script>
+    <script src="{{ asset('js/progressbar.min.js') }}"></script>
+    <script src="{{ asset('js/ion.rangeSlider.min.js') }}"></script>
+    <script src="{{ asset('js/chosen.jquery.min.js') }}"></script>
+    <script src="{{ asset('js/jQuery.Brazzers-Carousel.js') }}"></script>
+    <script src="{{ asset('js/plugins.js') }}"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDhAYvx0GmLyN5hlf6Uv_e9pPvUT3YpozE"></script>
-    <script src="{{asset('js/gmap.js')}}"></script>
-    <script src="{{asset('js/faq.js')}}"></script>
+    <script src="{{ asset('js/gmap.js') }}"></script>
+    <script src="{{ asset('js/faq.js') }}"></script>
     <!-- jQuery plugins/scripts - end -->
 
-    </body>
-    </html>
+</body>
+
+</html>
