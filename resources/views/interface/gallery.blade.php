d683bd0efb9e47747aa8cc9a00283ee646a92973
dwiriad <99604497+dwiriad@users.noreply.github.com>
2022-10-25 22:57:55 +0700

    edit

--- resources/views/interface/gallery.blade.php
+++ resources/views/interface/gallery.blade.php
@@ -123,7 +123,7 @@
 
         <!-- Pagination - start -->
         <ul class="pagi">
-            <li class="active">{{$products->links()}}</li>
+            <li class="active">{{$post->links()}}</li>
         </ul>
         <!-- Pagination - end -->
     </div>
